f1dcd582fc1c29406e523992fa4a10c8212e390b
skaushika26 <23csea16kaushika.s@gmail.com>
2025-11-21 12:22:45 +0530

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './App.css';
-console.log("URL:", import.meta.env.VITE_SUPABASE_URL);
+console.log("URL", import.meta.env.VITE_SUPABASE_URL);
 console.log("KEY:", import.meta.env.VITE_SUPABASE_ANON_KEY);
 
 
